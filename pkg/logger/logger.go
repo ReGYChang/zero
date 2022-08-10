0b7882f2ccf640b3baecbbb563bfc646e400ee7e
ReGYChang <p714140432@gmail.com>
2022-08-10 22:15:33 +0800

    feat: add logger pkg with zerolog

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,33 @@
 package logger
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"zero/pkg/config"
+)
+
+func SetupLogger() {
+	switch *config.AppConfig.LogLevel {
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "disabled":
+		zerolog.SetGlobalLevel(zerolog.Disabled)
+	}
+
+	log.Logger = log.Output(
+		zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: time.RFC3339,
+		},
+	).With().Caller().Logger()
+}
