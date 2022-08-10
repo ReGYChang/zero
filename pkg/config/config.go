98e6c570bf18fbd526d95a72aaeddce931267252
ReGYChang <p714140432@gmail.com>
2022-08-10 22:15:17 +0800

    feat: add config pkg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+type appConfig struct {
+	// General configuration
+	Env      *string
+	LogLevel *string
+
+	// Database configuration
+	DatabaseDSN *string
+
+	// HTTP/gRPC configuration
+	Type *string
+	Port *int
+
+	// Token configuration
+	TokenSigningKey         *string
+	TokenExpiryDurationHour *int
+	TokenIssuer             *string
+}
+
+var (
+	AppConfig = &appConfig{}
+)
