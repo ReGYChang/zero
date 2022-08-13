4188049d531ac9e720b649473d36fa1cc49eb7c9
ReGYChang <p714140432@gmail.com>
2022-08-13 22:58:50 +0800

    test: add benchmark test case

--- pkg/utils/worker_pool_test.go
+++ pkg/utils/worker_pool_test.go
@@ -1,20 +1,25 @@
 package utils
 
 import (
-	"fmt"
-	"strconv"
+	"encoding/json"
 	"testing"
 )
 
 type mockJob struct {
-	payload string
+	payload []byte
 }
 
 func (m *mockJob) Execute() {
-	fmt.Println(m.payload)
+	b := m.payload
+	for i := 0; i < 100; i++ {
+		var mt map[string]interface{}
+
+		_ = json.Unmarshal(b, &mt)
+		b, _ = json.Marshal(mt)
+	}
 }
 
-func TestWorkerPool(t *testing.T) {
+func Test_WorkerPool(t *testing.T) {
 	var request = 1000
 
 	d := NewDispatcher(100, 100)
@@ -22,9 +27,57 @@ func TestWorkerPool(t *testing.T) {
 
 	for i := 0; i < request; i++ {
 		j := &mockJob{
-			payload: strconv.Itoa(i),
+			payload: []byte(`
+				"person": {
+					"name": {
+					  "first": "Leonid",
+					  "last": "Bugaev",
+					  "fullName": "Leonid Bugaev"
+					},
+					"github": {
+					  "handle": "buger",
+					  "followers": 109
+					},
+					"avatars": [
+					  { "url": "https://avatars1.githubusercontent.com/u/14009?v=3&s=460", "type": "thumbnail" }
+					]
+				  },
+				  "company": {
+					"name": "Acme"
+				  }
+			`),
 		}
 		JobQueue <- j
 		t.Log("Operation successful")
+	}
+}
+
+func Benchmark_Worker_Pool(b *testing.B) {
+	d := NewDispatcher(100, 100)
+	d.Run()
+
+	for i := 0; i < b.N; i++ {
+		j := &mockJob{
+			payload: []byte(`
+				"person": {
+					"name": {
+					  "first": "Regy",
+					  "last": "Chang",
+					  "fullName": "Handsome Guy"
+					},
+					"github": {
+					  "handle": "regy",
+					  "followers": 3
+					},
+					"avatars": [
+					  { "url": "https://avatars1.githubusercontent.com/u/14009?v=3&s=460", "type": "thumbnail" }
+					]
+				  },
+				  "company": {
+					"name": "NexAIoT"
+				  }
+			`),
+		}
+		JobQueue <- j
 	}
 }
